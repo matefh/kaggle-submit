1a3a294937611592dc8c124c75d8851d28e984b5
Mohammad Atef <matefh@gmail.com>
2016-06-20 11:44:00 +0200

    Handling missing options

--- submit.js
+++ submit.js
@@ -2,6 +2,7 @@
 
 var program = require('commander');
 var exports = require('exports');
+var _ = require('lodash');
 
 var phantomjs = require('phantomjs');
 var path = require('path');
@@ -12,39 +13,34 @@ var YAML = require('js-yaml');
 var fs = require('fs');
 var conf = YAML.safeLoad(fs.readFileSync('config.yml'));
 
-var competition = conf.competition, username = conf.username, password = conf.password;
-
 exports.printMsg = function() {
   console.log("Use me to submit your solutions to Kaggle from command-line.");
 }
 
 program
   .version('0.0.1')
-  .option('-c, --competition <competition>', 'the comp')
-  .option('-u, --username <username>', 'uzar name')
-  .option('-p, --password <password>', 'bassword')
-  .option('-S, --submission <submission>', 'subz')
+  .option('-c, --competition <competition>', 'kaggle competition (example: titanic, as in https://www.kaggle.com/c/titanic/')
+  .option('-u, --username <username>', 'your kaggle username')
+  .option('-p, --password <password>', 'your kaggle password')
+  .option('-s, --submission <submission>', 'path to your submission file (csv/zip/gz/rar/7z)')
   .parse(process.argv);
+var inputs = [
+  program.competition,
+  program.username,
+  program.password,
+  program.submission
+];
 
-if (program.competition)
-  competition = program.competition;
+if (_.some(inputs, _.isNil))
+  program.help();
 
-if (program.username)
-  username = program.username;
 
-if (program.password)
-  password = program.password;
-
-var submission = program.submission;
-
-var childArgs = [
-  "--ssl-protocol=any",
-  path.join(__dirname, 'phantom_submit.js'),
-  competition,
-  username,
-  password,
-  submission
-]
+var childArgs = _.concat(
+  [
+    "--ssl-protocol=any",
+    path.join(__dirname, 'phantom_submit.js')
+  ],
+  inputs);
 
 childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
   console.log(stdout);
